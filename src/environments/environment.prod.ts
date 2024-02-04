97edbe32833fd3a172247db658d83d0dcec28335
Dean Luckham <dean@deanluckham.dev>
2024-02-04 22:42:57 +0000

    Build
    Signed-off-by: Dean Luckham <dean@deanluckham.dev>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true,
-  title: 'production',
+  production: false,
+  title: 'dev',
+  s3Url: 'https://s3.eu-west-2.amazonaws.com/deanluckham.dev/',
   s3ImagesUrl: 'https://s3.eu-west-2.amazonaws.com/deanluckham.dev/images/',
 };
