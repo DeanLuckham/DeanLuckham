86525b3808d1e26df16544ed675b98991323ddc7
Dean Luckham <dean@deanluckham.dev>
2024-04-17 16:57:40 +0100

    Remove Unused

--- src/app/odin/library/book.ts
+++
@@ -1,9 +0,0 @@
-export class Book {
-  constructor(
-    public title: string = "Unknown",
-    public author: string = "Unknown",
-    public pages: number = 0,
-    public read: boolean = false
-  ) {
-  }
-}
